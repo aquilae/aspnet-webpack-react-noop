fb83d01ce411e10d6186533610c8c3323d2cadc3
aquilae <upsnake@gmail.com>
2018-03-07 02:04:05 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+console.warn('ASPNET-WEBPACK-REACT REPLACED 3');
+exports.addReactHotModuleReplacementConfig = function () { console.warn('REPLACED 3-1'); };
+exports.addReactHotModuleReplacementBabelTransform = function () { console.warn('REPLACED 3-2'); };

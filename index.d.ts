fa9565937fd981faf6788f18d00def895c578294
aquilae <upsnake@gmail.com>
2018-03-07 02:12:34 +0300

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,5 @@
+/// <reference types="webpack" />
+import * as webpack from 'webpack';
+
 export declare function addReactHotModuleReplacementConfig(webpackConfig: webpack.Configuration): void;
 export declare function addReactHotModuleReplacementBabelTransform(webpackConfig: webpack.Configuration): void;
